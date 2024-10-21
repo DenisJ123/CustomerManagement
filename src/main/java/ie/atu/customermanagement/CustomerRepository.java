52be311e52dc6393a3a8f78c0c8533de0164a525
DenisJ123 <114908491+DenisJ123@users.noreply.github.com>
2024-10-21 12:46:46 +0100

    Delete src/main/java/ie/atu/customermanagement/CustomerRepository.java

--- src/main/java/ie/atu/customermanagement/CustomerRepository.java
+++
@@ -1,9 +0,0 @@
-package ie.atu.customermanagement;
-
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface CustomerRepository extends JpaRepository<CustomerProduct, Long>
-{
-    CustomerProduct findByCustomerCode(int customerCode);
-}
